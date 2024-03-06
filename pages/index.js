3ca7e11a9c176ebf6725b5440cb5a10e01ca4e24
dmitryi698 <dima2prog@gmail.com>
2024-03-06 12:38:10 +0400

    Fix: fix selected value if do not find value from query

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import App from '../src/components/App.js';
-import 'antd/dist/antd.css';
-import 'semantic-ui-css/semantic.min.css';
+import App from "../src/components/App.js";
+import "antd/dist/antd.css";
+import "semantic-ui-css/semantic.min.css";
 
 export default function page() {
   return <App />;
