c929b33d1084eab752edb42ab8acf322188430ae
dmitryi698 <dima2prog@gmail.com>
2024-11-21 12:25:23 +0400

    Fix:-Убрал из фильтров параметры с name

--- src/resources/ModificationResource.mjs
+++ src/resources/ModificationResource.mjs
@@ -1,28 +1,21 @@
 import Api from "../Api.js";
 import Resource from "./Resource.mjs";
-/* eslint-disable no-unused-vars -- Отключаем eslint no-unused-vars */
 
 export default class ModificationResource extends Resource {
   /**
    * @param {Object} filters
-   * @param {string} filters.modelName
-   * @param {string} filters.bodyName
    * @param {string} autocatalogsUrl
    * @returns {Promise<ModificationResource[]>}
    */
-  static async get({ modelName, bodyName, ...filters }, autocatalogsUrl) {
-    if (filters.modelCode || filters.modelId || modelName) {
-      const result = await Api.get(
-        autocatalogsUrl ? `${autocatalogsUrl}/modifications` : "/autocatalogs/modifications",
-        {
-          withTransmission: true,
-          ...filters,
-        },
-      );
+  static async get(filters, autocatalogsUrl) {
+    if (filters.modelCode) {
+      const result = await Api.get(`${autocatalogsUrl}/modifications`, {
+        withTransmission: true,
+        ...filters,
+      });
 
       return ModificationResource.map(result.body || []);
     }
     return [];
   }
 }
-/* eslint-enable no-unused-vars -- Возвращаем eslint no-unused-vars */
